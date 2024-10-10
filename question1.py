1b52988665e02e1125f3ef05f9f576fdece53bb9
unknown <fayimarahuman2002@gmail.com>
2024-10-10 10:44:52 +0300

    test one

---
+++ question1.py
@@ -0,0 +1,11 @@
+#create a program that calculate the area of a triangle .note in exams questions must not be rewritten
+#question 1(a)
+base=6
+height=10
+area_of_a_triangle=1/2*base*height
+print(area_of_a_triangle)
+#OR
+base=int(input("enter the base of a triangle"))
+height=int(input("enter the height of a triangle"))
+
+
---
+++ question2.py
@@ -0,0 +1,5 @@
+#using a function create a program that calculate the volume of a cylinder.
+radius=int(input("enter the radius of a cylinder :"))
+height=int(input("enter the height of the cylinder:"))
+volume_of_a_cylinder=22/7*radius**2*height
+print(f"the volume of a cylinder is {volume_of_a_cylinder} ")
